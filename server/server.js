7dd906591113e542eb0fb7da012c851d9c18c410
Hiren Gondhiya <hirengondhiya@gmail.com>
2017-06-28 14:49:01 +1000

    Greet new user and Inform others

--- server/server.js
+++ server/server.js
@@ -16,19 +16,31 @@ app.use(express.static(publicFolder));
 
 io.on('connection', (socket) => {
     console.log('New user connected.');
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to chat app.'
+    });
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New User Joined'
+    });
     const createMessageListener = function createMessageListenerMethod(message) {
         console.log(`Create message: ${JSON.stringify(message, undefined, 2)}`);
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime().toString()
-        });
+        // io.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime().toString()
+        // });
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime().toString()
+        // });
     };
 
     const disconnectListener = function disconnectListenerMethod() {
         console.log('Disconnected from client.');
     };
-
     socket.on('createMessage', createMessageListener);
 
     socket.on('disconnect', disconnectListener);
